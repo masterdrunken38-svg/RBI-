065500c8a3693129619365da9bf9cc0b22d59c40
masterdrunken38-svg <masterdrunken38@gmail.com>
2026-04-22 20:10:30 +0530

    Update chat.js

--- netlify/functions/chat.js
+++ netlify/functions/chat.js
@@ -67,7 +67,7 @@ exports.handler = async function(event) {
     reqBody.tools = [{ googleSearch: {} }];
   }
 
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${KEY}`;
+  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${KEY}`;
 
   try {
     const resp = await fetch(url, {
